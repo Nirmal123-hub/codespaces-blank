b70d70429a4bf0cffd3c03e18d3c0fd9e125dc96
Nirmal123-hub <149949584+Nirmal123-hub@users.noreply.github.com>
2024-03-07 16:00:44 +0000

    few changes

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,17 +1,17 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
-const weatherRoute_js_1 = __importDefault(require('./routes/weatherRoute.js'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const weatherRoute_js_1 = __importDefault(require("./routes/weatherRoute.js"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 //our new route
 app.use('/api/weather', weatherRoute_js_1.default);
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
--- dist/src/server.ts
+++ dist/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import weatherRoute from './routes/weatherRoute.js';
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 //our new route
 app.use('/api/weather', weatherRoute);
